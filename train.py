081525c132bc8d9aa1c31f11b03a1482b1696e71
XDong18 <1141191813@qq.com>
2020-09-13 00:39:08 +0800

    update

--- train.py
+++ train.py
@@ -40,7 +40,7 @@ def setup(args):
     cfg.DATASETS.TEST = ("bdd100k_val",)
     cfg.DATALOADER.NUM_WORKERS = 16
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")  # Let training initialize from model zoo
-    cfg.SOLVER.IMS_PER_BATCH = 4
+    cfg.SOLVER.IMS_PER_BATCH = 16
     cfg.SOLVER.BASE_LR = 0.00025  #TODO verify
     cfg.SOLVER.MAX_ITER = 10000  #TODO verify
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE =512 #TODO verify
@@ -57,7 +57,10 @@ def setup(args):
 
 def main(args):
     cfg = setup(args)
-
+    # dataset
+    register_coco_instances("bdd100k_train", {}, "train_coco.json", "/data5/bdd100k/images/100k/train")
+    register_coco_instances("bdd100k_val", {}, "val_coco.json", "/data5/bdd100k/images/100k/val")
+    
     if args.eval_only:
         model = Trainer.build_model(cfg)
         DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
@@ -73,9 +76,8 @@ def main(args):
 
 if __name__ == "__main__":
     # dataset registration
-    global DatasetCatalog, MetadataCatalog
-    register_coco_instances("bdd100k_train", {}, "train_coco.json", "/data5/bdd100k/images/100k/train")
-    register_coco_instances("bdd100k_val", {}, "val_coco.json", "/data5/bdd100k/images/100k/val")
+    # global DatasetCatalog, MetadataCatalog
+
 
     args = default_argument_parser().parse_args()
     print("Command Line Args:", args)
