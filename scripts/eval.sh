ca6b57d27f1b490b5b27cb88816cfa4eb481de2c
XDong18 <1141191813@qq.com>
2020-09-14 15:24:23 +0800

    update

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=5,6,7,8
-python train.py --num-gpus 4 --eval-only MODEL.WEIGHTS ./bdd100k_fasterrcnn/model_0004999.pth dist_url auto
+python train.py --num-gpus 4 --dist_url auto --eval-only MODEL.WEIGHTS ./bdd100k_fasterrcnn/model_0004999.pth 
