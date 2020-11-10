f1bed2ac5c80de685f755fe31a13e082785d72f8
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-11 05:38:58 +0800

    cc

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0,9
-python train_bdd.py --num-gpus 2 --dist-url auto --eval-only MODEL.WEIGHTS ./bdd100k_faster_rcnn_R_50_FPN_1x/bdd100k_faster_rcnn_R_50_FPN_1x-0b30383c.pth 
+python train_bdd.py --num-gpus 2 --dist-url auto --eval-only MODEL.WEIGHTS ./bdd100k_retinanet_R_50_FPN_1x/bdd100k_retinanet_R_50_FPN_1x-303595e5.pth 
