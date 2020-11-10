2648f7d1b1084a2fba54133ba31334cbe7a30da2
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-11 06:56:25 +0800

    cc

--- info_eval.py
+++ info_eval.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     eval_json_name = 'bdd100k' + '-' 'detectron2' + '-' + 'faster_rcnn_r50_fpn-1x' + '-' + 'eval' + '.json'
     eval_json_name = os.path.join(out_dir, eval_json_name)
 
-    data = {'classwise_AP': [['person', '0.239'], ['rider', '0.187'], ['car', '0.411'], ['bus', '0.400'], ['truck', '0.382'], ['bike', '0.192'], ['motor', '0.183'], ['traffic light', '0.166'], ['traffic sign', '0.298'], ['train', '0.001']], 
-    'bbox_mAP': 0.246, 'bbox_mAP_50': 0.498, 'bbox_mAP_75': 0.211, 
-    'bbox_mAP_s': 0.086, 'bbox_mAP_m': 0.305, 'bbox_mAP_l': 0.473
+    data = {'classwise_AP': [['person', '0.329'], ['rider', '0.243'], ['car', '0.504'], ['bus', '0.458'], ['truck', '0.443'], ['bike', '0.242'], ['motor', '0.230'], ['traffic light', '0.258'], ['traffic sign', '0.384'], ['train', '0.000']], 
+    'bbox_mAP': 0.309, 'bbox_mAP_50': 0.572, 'bbox_mAP_75': 0.285, 
+    'bbox_mAP_s': 0.147, 'bbox_mAP_m': 0.358, 'bbox_mAP_l': 0.518
     }
 
     with open(eval_json_name, 'w') as f:
