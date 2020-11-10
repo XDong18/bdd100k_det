ca876110a720a2669651a3de2d4a53b03439487f
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-11 07:09:54 +0800

    cc

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=6,7,8,9
-python train_bdd.py --num-gpus 4 --dist-url auto --eval-only MODEL.WEIGHTS ./bdd100k_retinanet_R_50_FPN_1x/bdd100k_retinanet_R_50_FPN_1x-303595e5.pth 
+python train_bdd.py --num-gpus 4 --dist-url auto --eval-only MODEL.WEIGHTS ./bdd100k_retinanet_new/Retinanet_r_101_fpn_1x-745c57cc.pth
