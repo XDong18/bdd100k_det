6e2cc3aade56a11138b109a5fe80ae9da848e38c
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-11 04:15:37 +0800

    cc

--- train_bdd.py
+++ train_bdd.py
@@ -3,15 +3,15 @@ import os
 from collections import OrderedDict
 import torch
 
-import detectron2.utils.comm as comm
-from detectron2.checkpoint import DetectionCheckpointer
-from detectron2.config import get_cfg
-from detectron2.data import MetadataCatalog, DatasetCatalog
-from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, hooks, launch
-from detectron2.evaluation import COCOEvaluator
-from detectron2.modeling import GeneralizedRCNNWithTTA
-from detectron2 import model_zoo
-from detectron2.data.datasets import register_coco_instances
+import dl_lib.utils.comm as comm
+from dl_lib.checkpoint import DetectionCheckpointer
+from dl_lib.config import get_cfg
+from dl_lib.data import MetadataCatalog, DatasetCatalog
+from dl_lib.engine import DefaultTrainer, default_argument_parser, default_setup, hooks, launch
+from dl_lib.evaluation import COCOEvaluator
+from dl_lib.modeling import GeneralizedRCNNWithTTA
+from dl_lib import model_zoo
+from dl_lib.data.datasets import register_coco_instances
 
 
 class Trainer(DefaultTrainer):
@@ -24,7 +24,7 @@ class Trainer(DefaultTrainer):
     @classmethod
     def build_evaluator(cls, cfg, dataset_name, output_folder=None):
         if output_folder is None:
-            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference_test")
         return COCOEvaluator(dataset_name, cfg, True, output_folder)
 
 
@@ -35,13 +35,13 @@ def setup(args):
     cfg = get_cfg()
 
     # set config file
-    cfg.merge_from_file("configs/BDD100K-Detection/faster_rcnn/faster_rcnn_R_101_C4_1x_crop.yaml")
+    cfg.merge_from_file("bdd100k_faster_rcnn_R_101_FPN_1x/config.yaml")
     cfg.DATASETS.TRAIN = ("bdd100k_train",)
-    cfg.DATASETS.TEST = ("bdd100k_val",)
+    cfg.DATASETS.TEST = ("bdd100k_test",)
     # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")  # Let training initialize from model zoo
 
 
-    cfg.OUTPUT_DIR = './bdd100k_faster_rcnn_R_101_C4_1x'
+    cfg.OUTPUT_DIR = './bdd100k_faster_rcnn_R_101_FPN_1x'
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
     # new added solver arguments
     cfg.SOLVER.CHECKPOINT_PERIOD = 5000
@@ -59,7 +59,7 @@ def main(args):
     cfg = setup(args)
     # dataset
     register_coco_instances("bdd100k_train", {}, "train_coco.json", "/shared/xudongliu/bdd100k/100k/train")
-    register_coco_instances("bdd100k_val", {}, "val_coco.json", "/shared/xudongliu/bdd100k/100k/val")
+    register_coco_instances("bdd100k_test", {}, "/shared/xudongliu/bdd100k/labels/bdd100k_labels_images_det_coco_test.json", "/shared/xudongliu/bdd100k/100k/test")
 
     if args.eval_only:
         model = Trainer.build_model(cfg)
