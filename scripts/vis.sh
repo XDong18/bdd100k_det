407055cb236affd01f0aca43996eeac9355991de
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-09 10:18:52 +0800

    cc

--- scripts/vis.sh
+++ scripts/vis.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=4,5,8
 python vis/demo.py --config-file configs/BDD00K-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml \
   --input /data5/bdd100k/images/10k/val/* \
-  --output output/mask_rcnn_R_50_FPN_3x_val/ \
+  --output show/mask_rcnn_R_50_FPN_3x_val/ \
   --opts SOLVER.IMS_PER_BATCH 12 MODEL.WEIGHTS /shared/xudongliu/code/bdd100k_det/bdd100k_maskrcnn_720_8bs_0.01_3x/model_final.pth
