70e9897493cb54266bff8c1f4e84b4798ad9d725
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-09 10:51:14 +0800

    cc

--- auto_html.py
+++ auto_html.py
@@ -1,8 +1,8 @@
 import os
 
-img_dir = '/shared/xudongliu/code/bdd100k_det/show/mask_rcnn_R_50_FPN_3x_val'
+img_dir = 'show/mask_rcnn_R_50_FPN_3x_val'
 img_list = os.listdir(img_dir)
-out_fn = 'html/mask_rcnn_R_50_FPN_3x_val.html'
+out_fn = 'show/mask_rcnn_R_50_FPN_3x_val.html'
 with open (out_fn, 'w') as f:
     f.write('<html>\n')
     f.write('<body>\n')
@@ -11,7 +11,7 @@ with open (out_fn, 'w') as f:
     for img in img_list:
         name = "<p>" + img + "</p>\n"
         f.write(name)
-        new_line = "<img src='" + os.path.join(img_dir, img) + "'>\n"
+        new_line = "<img src='" + os.path.join('mask_rcnn_R_50_FPN_3x_val', img) + "'>\n"
         f.write(new_line)
 
     # f.write("<img src='" + "test.jpg" + "'>\n")
