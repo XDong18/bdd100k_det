addd127b0712cd5580a53094d055ed364fd3863a
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-09 10:53:42 +0800

    cc

--- auto_html.py
+++ auto_html.py
@@ -1,15 +1,15 @@
 import os
 
 img_dir = 'show/mask_rcnn_R_50_FPN_3x_val'
-img_list = os.listdir(img_dir)
+img_list = sorted(os.listdir(img_dir))
 out_fn = 'show/mask_rcnn_R_50_FPN_3x_val.html'
 with open (out_fn, 'w') as f:
     f.write('<html>\n')
     f.write('<body>\n')
     f.write('<h1>mask_rcnn_R_50_FPN_3x_val</h1>\n')
 
-    for img in img_list:
-        name = "<p>" + img + "</p>\n"
+    for i, img in enumerate(img_list):
+        name = "<p>" + str(i) + ' ---- ' + img + "</p>\n"
         f.write(name)
         new_line = "<img src='" + os.path.join('mask_rcnn_R_50_FPN_3x_val', img) + "'>\n"
         f.write(new_line)
