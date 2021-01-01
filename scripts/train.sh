87cb0840d3d3857ef530efecec05420345548a85
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2021-01-02 00:46:36 +0800

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-python train_bdd.py --num-gpus 8
+python train_tensormask.py --num-gpus 8
--- train_tensormask.py
+++ train_tensormask.py
@@ -29,13 +29,13 @@ def setup(args):
     cfg = get_cfg()
     add_tensormask_config(cfg)
     # set config file
-    cfg.merge_from_file("configs/BDD00K-InstanceSegmentation/tensormask_r50_3x_bs8.yaml")
+    cfg.merge_from_file("configs/BDD00K-InstanceSegmentation/tensormask_r101_3x_multi_scale_bs16_bdd100k.yaml")
     cfg.DATASETS.TRAIN = ("bdd100k_train",)
     cfg.DATASETS.TEST = ("bdd100k_test",)
     # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")  # Let training initialize from model zoo
 
 
-    cfg.OUTPUT_DIR = './tensormask_r50_3x_bs8'
+    cfg.OUTPUT_DIR = './tensormask_r101_3x_multi_scale_bs16_bdd100k'
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
     # new added solver arguments
     cfg.SOLVER.CHECKPOINT_PERIOD = 500
