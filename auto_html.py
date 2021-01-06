a0a136d2b6fe56dd10bece055848a2df70ce6cb7
XDong18 <liuxd1215@gmail.com>
2021-01-06 16:47:27 +0800

    update

--- auto_html.py
+++ auto_html.py
@@ -1,17 +1,17 @@
 import os
 
-img_dir = 'show/mask_rcnn_R_50_FPN_3x_val'
+img_dir = 'show/mask_rcnn_r_101_fpn_yanzaho'
 img_list = sorted(os.listdir(img_dir))
-out_fn = 'show/mask_rcnn_R_50_FPN_3x_val.html'
+out_fn = 'show/mask_rcnn_r_101_fpn_yanzaho.html'
 with open (out_fn, 'w') as f:
     f.write('<html>\n')
     f.write('<body>\n')
-    f.write('<h1>mask_rcnn_R_50_FPN_3x_val</h1>\n')
+    f.write('<h1>mask_rcnn_r_101_fpn_yanzaho</h1>\n')
 
     for i, img in enumerate(img_list):
         name = "<p>" + str(i) + ' ---- ' + img + "</p>\n"
         f.write(name)
-        new_line = "<img src='" + os.path.join('mask_rcnn_R_50_FPN_3x_val', img) + "'>\n"
+        new_line = "<img src='" + os.path.join('mask_rcnn_r_101_fpn_yanzaho', img) + "'>\n"
         f.write(new_line)
 
     # f.write("<img src='" + "test.jpg" + "'>\n")
