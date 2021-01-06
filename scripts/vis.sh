3f1840e0e9379a94671140d2659cf49b5987bcc9
XDong18 <liuxd1215@gmail.com>
2021-01-06 16:43:56 +0800

    update

--- scripts/vis.sh
+++ scripts/vis.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7,8
 python vis/demo.py --config-file configs/BDD00K-InstanceSegmentation/mask_rcnn_r_101_fpn_yanzaho.yaml \
-  --input /data5/bdd100k/images/10k/val/* \
+  --input /shared/xudongliu/bdd100k/10k/val/* \
   --output show/mask_rcnn_r_101_fpn_yanzaho/ \
   --opts SOLVER.IMS_PER_BATCH 8 MODEL.WEIGHTS /shared/xudongliu/code/bdd100k_det/mask_rcnn_r_101_fpn_yanzaho/model_final.pth
