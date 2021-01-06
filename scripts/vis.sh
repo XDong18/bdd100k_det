d6d22a740a28851524603f2c07302ed50ca533ee
XDong18 <liuxd1215@gmail.com>
2021-01-06 16:42:31 +0800

    update

--- scripts/vis.sh
+++ scripts/vis.sh
@@ -1,5 +1,5 @@
-export CUDA_VISIBLE_DEVICES=4,5,8
-python vis/demo.py --config-file configs/BDD00K-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml \
+export CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7,8
+python vis/demo.py --config-file configs/BDD00K-InstanceSegmentation/mask_rcnn_r_101_fpn_yanzaho.yaml \
   --input /data5/bdd100k/images/10k/val/* \
-  --output show/mask_rcnn_R_50_FPN_3x_val/ \
-  --opts SOLVER.IMS_PER_BATCH 12 MODEL.WEIGHTS /shared/xudongliu/code/bdd100k_det/bdd100k_maskrcnn_720_8bs_0.01_3x/model_final.pth
+  --output show/mask_rcnn_r_101_fpn_yanzaho/ \
+  --opts SOLVER.IMS_PER_BATCH 8 MODEL.WEIGHTS /shared/xudongliu/code/bdd100k_det/mask_rcnn_r_101_fpn_yanzaho/model_final.pth
