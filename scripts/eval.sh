e4323f6bbceca07cf8338fdcd9b0885863cb9e8f
XDong18 <liuxd1215@gmail.com>
2021-01-13 10:03:06 +0800

    update

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,2 +1,2 @@
-export CUDA_VISIBLE_DEVICES=6,7,8,9
-python train_bdd.py --num-gpus 4 --dist-url auto --eval-only MODEL.WEIGHTS ./bdd100k_retinanet_new/Retinanet_r_101_fpn_1x-745c57cc.pth
+export CUDA_VISIBLE_DEVICES=1,8,9
+python train_bdd.py --num-gpus 3 --dist-url auto --eval-only MODEL.WEIGHTS ./mask_rcnn_r_101_fpn_yanzaho/model_0014499.pth
